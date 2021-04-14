82e7425619ded9e1d479ca73d73a52603aa8613c
Shrikant Pardhi <dynast.dev@gmail.com>
2021-04-14 07:19:59 +0530

    initil commit

---
+++ src/AbstractClassDemo.java
@@ -0,0 +1,31 @@
+abstract class Demo{
+	
+	Demo (){}
+	Demo (String name){}
+	
+	abstract void display();
+	
+	void displaynon() {
+		System.out.println("non abtract method");
+	}
+	
+}
+
+public class AbstractClassDemo extends Demo {
+	
+	AbstractClassDemo(){
+		super();
+	}
+	void display() {
+		System.out.println("abtract method");
+	}
+
+	public static void main(String[] args) {
+		
+		AbstractClassDemo obj =  new AbstractClassDemo();
+		
+		obj.display();
+		obj.displaynon();
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,60 @@
+class ParentAnimal{
+	public void eat(String name) {
+		System.out.println("Name "+  name);
+	}
+}
+
+class Animal extends ParentAnimal {
+	String name;
+	
+	public Animal() {
+		System.out.println("called contructor using super");
+	}
+	
+	public Animal(String tommy) {
+		System.out.println("super with arg "+ tommy);
+	}
+	
+	public void eat() {
+		System.out.println("I can dog eat");
+	}
+	public void eat(String name) {
+		super.eat(name);
+	}
+	public void eate(int name) {
+		
+	}
+} 
+
+class Dog extends Animal{
+	
+	Dog(String tommy){
+		super(tommy);
+		
+	}
+	
+	@Override
+	public void eat() {
+		super.eat();
+//	    System.out.println("dog eat");
+	}
+} 
+
+class Cat extends Animal{
+	@Override
+	public void eat(String name) {
+		super.eat(name);
+	}
+	public void eat(int name) {
+		super.eate(name);
+	}
+	
+}
+class Main {
+	  public static void main(String[] args) {
+	    Dog labrador = new Dog("tommy");
+	    labrador.eat();
+	    Cat c = new Cat();
+	    c.eat("Meu");
+	  }
+	}
